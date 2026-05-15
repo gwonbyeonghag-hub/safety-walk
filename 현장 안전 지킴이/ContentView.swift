d82fd08c34746618950ce5505182ba0b639968c9
ByeongHag Gwon <gwonbyeonghag@gmail.com>
2026-05-15 21:02:29 +0900

    Initial Commit

---
+++ 현장 안전 지킴이/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  현장 안전 지킴이
+//
+//  Created by 권병학 on 5/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
